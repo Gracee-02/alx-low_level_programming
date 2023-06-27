32d8e24df2bfaada7c7ac57891cd90f7f24e8cc1
Grace <otuyagrace@gmail.com>
2023-06-27 12:10:16 -0700

    reset using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that take a pointer to an int
+ * as parameter and uptades tha value
+ * it points to
+ *
+ * Return: nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# Pointers Arrays Strings
+
+Learning what pointers are and how to use them
+
+WHat are arrays and how to use them
+
+How to usee strings and how to manipulate them
